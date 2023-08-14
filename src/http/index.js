2fb20b9a0b89ea1e73e6817f269b32d244d18c9a
goodmartian <credocoder@gmail.com>
2023-08-14 12:34:21 +0300

    feat: front-end build added

--- src/http/index.js
+++ src/http/index.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const $host = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
-  withCredentials: true
   // headers: {
   //   'Content-Type': 'application/json',
   //   Accept: 'application/json',
